8ffa0df944e2d3f20cb4464eb4b09e299e8f8cb2
parkchaewon <xkdldjaos20@gmail.com>
2023-08-08 11:14:14 +0900

    user

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
+require("dotenv").config();
+
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
+const { SESSION_SECRET_KEY } = process.env;
+const user = require("./routes/user");
 
 const PORT = 5000;
+
+const dotenv = require("dotenv");
+dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(express.urlencoded({extended:false}));
-app.use(express.static("assets"))
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("assets"));
 
-app.use('/api', []);
+app.use("/api", [user]);
+app.use(
+  session({
+    secret: SESSION_SECRET_KEY,
+    resave: false,
+    rolling: true,
+    saveUninitialized: false,
+    store: new MemoryStore({ checkPeriod: 1000 * 60 * 60 }),
+    cookie: {
+      maxAge: 1000 * 60 * 60,
+    },
+  })
+);
 
 app.listen(PORT, () => {
-  console.log(PORT, '포트 번호로 서버가 실행되었습니다.');
+  console.log(PORT, "포트 번호로 서버가 실행되었습니다.");
 });
--- models/cards.js
+++ models/cards.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Cards extends Model {
     /**
@@ -11,85 +9,91 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.belongsTo(models.Users, { //  1:N 관계 설정을 합니다.
-        targetKey: 'UserId', 
-        foreignKey: 'userId', 
+      this.belongsTo(models.Users, {
+        //  1:N 관계 설정을 합니다.
+        targetKey: "UserId",
+        foreignKey: "userId",
       });
-      this.belongsTo(models.Colums, { //  1:N 관계 설정을 합니다.
-        targetKey: 'ColumnId', 
-        foreignKey: 'columnId', 
+      this.belongsTo(models.Columns, {
+        //  1:N 관계 설정을 합니다.
+        targetKey: "ColumnId",
+        foreignKey: "columnId",
       });
-      this.hasOne(models.Comments, { // 1:N 관계 설정을 합니다.
-        sourceKey: 'CardId', 
-        foreignKey: 'cardId', 
+      this.hasOne(models.Comments, {
+        // 1:N 관계 설정을 합니다.
+        sourceKey: "CardId",
+        foreignKey: "cardId",
       });
     }
   }
-  Cards.init({
-    CardId: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: DataTypes.INTEGER
-    },
-    userId: {
-      allowNull: false,
-      type: DataTypes.INTEGER,
-      references:{
-        model:'Users',
-        key:'UserId',
+  Cards.init(
+    {
+      CardId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      userId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: "Users",
+          key: "UserId",
+        },
+      },
+      boardId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: "Boards",
+          key: "BoardId",
+        },
+      },
+      columnId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: "Columns",
+          key: "ColumnId",
+        },
+      },
+      title: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      content: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      cardState: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+      cardColor: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      endDate: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: new Date(),
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
       },
     },
-    boardId: {
-      allowNull: false,
-      type: DataTypes.INTEGER,
-      references:{
-        model:'Boards',
-        key:'BoardId',
-      },
-    },
-    columnId: {
-      allowNull: false,
-      type: DataTypes.INTEGER,
-      references:{
-        model:'Columns',
-        key:'ColumnId',
-      },
-    },
-    title: {
-      allowNull: false,
-      type: DataTypes.STRING
-    },
-    content: {
-      allowNull: false,
-      type: DataTypes.STRING
-    },
-    cardState: {
-      allowNull: false,
-      type: DataTypes.INTEGER
-    },
-    cardColor: {
-      allowNull: false,
-      type: DataTypes.STRING
-    },
-    endDate: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: new Date()
-    },
-    createdAt: {
-      allowNull: false,
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW,
-    },
-    updatedAt: {
-      allowNull: false,
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW,
+    {
+      sequelize,
+      modelName: "Cards",
     }
-  }, {
-    sequelize,
-    modelName: 'Cards',
-  });
+  );
   return Cards;
 };
