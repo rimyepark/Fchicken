2c4b1ccfac0f160d416c8471cf5b06067d456063
parkchaewon <xkdldjaos20@gmail.com>
2023-08-09 11:07:09 +0900

    병합

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -11,7 +11,6 @@ module.exports = async (req, res, next) => {
       return res.status(412).json({ message: "사용자 정보가 변조되어 로그아웃 되었습니다." });
     }
 
-    // if (!req.session.user) return res.redirect('../');
     next();
   } catch (error) {
     res.status(401).json({
