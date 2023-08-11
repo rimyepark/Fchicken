7fa1604427f7d7cd77e582a59e2c9bdcdc9a42f2
choichanggeun <choichanggeoun@gmail.com>
2023-08-12 03:53:50 +0900

    댓글 읽어오기 완료

--- public/js/comments.js
+++ public/js/comments.js
@@ -21,23 +21,25 @@ document.addEventListener("DOMContentLoaded", () => {
   };
 
   const getComments = async () => {
-    const cardId = 1; // 카드 ID를 수정하거나 동적으로 설정하세요.
-    const response = await fetch(`/cards/${cardId}/comments`);
+    const cardId = 1;
+    const response = await fetch(`api/cards/${cardId}/comments`);
     const data = await response.json();
     return data.data;
   };
+
   const createComment = async (content) => {
     const cardId = 1;
-    const response = await fetch(`api/cards/${cardId}/comments`, {
+    const response = await fetch(`/api/cards/${cardId}/comments`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ content }),
     });
+
     if (!response.ok) {
       console.error(`Error: ${response.status} ${response.statusText}`);
-      const responseText = await response.text(); // 서버 응답을 텍스트 형식으로 확인하십시오.
+      const responseText = await response.text();
       console.error(`Response Text: ${responseText}`);
-      throw new Error("Error creating comment"); // 에러를 발생시켜 응답이 실패하면 처리가 중지되도록 합니다.
+      throw new Error("Error creating comment");
     }
 
     return response.json();
