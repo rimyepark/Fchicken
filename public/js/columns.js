b1508a6ef3b152a88b7ea21fd35b9d08fe49a110
parkchaewon <xkdldjaos20@gmail.com>
2023-08-11 23:47:59 +0900

    .

---
+++ public/js/columns.js
@@ -0,0 +1,29 @@
+window.addEventListener("DOMContentLoaded", async function () {
+  try {
+    const response = await fetch("/api/columns");
+    const data = await response.json();
+
+    console.log(data);
+
+    const columnsList = document.getElementById("Columns");
+
+    for (const column of data) {
+      const columnName = column.columnName;
+
+      const temp_html = `
+    <li>
+    <div class="column">
+    <h2>${columnName}</h2>
+    <button class="add-card-btn">Add Card</button>
+    <div class="card">Card 1</div>
+    <div class="card">Card 2</div>
+    </div>
+    </li>
+    `;
+
+      columnsList.innerHTML += temp_html;
+    }
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+});
--- repositories/columns.repository.js
+++ repositories/columns.repository.js
@@ -3,21 +3,22 @@ const { Columns } = require("../models");
 class ColumnRepository {
   //칼럼 전체 조회 api
   findAllColumn = async () => {
-    const columns = await Columns.findAll();
-    return columns;
+    const Column = await Columns.findAll();
+    return Column;
   };
+
   //칼럼 조회 api
   findColumnById = async (ColumnId) => {
     const column = await Columns.findByPk(ColumnId);
     return column;
   };
   //칼럼 생성
-    createColumn = async (boardId,columnName,columnIndex) => {
-      const createColumn = await Columns.create({boardId,columnName,columnIndex });
-      return createColumn;
-    }
+  createColumn = async (boardId, columnName, columnIndex) => {
+    const createColumn = await Columns.create({ boardId, columnName, columnIndex });
+    return createColumn;
+  };
   // 칼럼 이름 수정 api
-  updateColumnName = async (ColumnId,columnName) => {
+  updateColumnName = async (ColumnId, columnName) => {
     const updateColumnData = await Columns.update({ columnName }, { where: { ColumnId } });
     return updateColumnData;
   };
@@ -28,10 +29,9 @@ class ColumnRepository {
   };
 
   //칼럼 스왑 api
-updateColumn(column) {
-  return column.save();
+  updateColumn(column) {
+    return column.save();
+  }
 }
-}
-
 
 module.exports = ColumnRepository;
