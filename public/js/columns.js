ce18cb774b3ed1afaab8bcbbf4b4bdd4430f9e70
yerim <sola4718@gmail.com>
2023-08-13 01:39:52 +0900

    칼럼 수정 기능 수정중

--- public/js/columns.js
+++ public/js/columns.js
@@ -9,12 +9,13 @@ window.addEventListener("DOMContentLoaded", async function () {
         if (columnsList) {
             for (const column of data) {
                 const columnName = column.columnName;
+                const ColumnId = column.columnId; // 칼럼 아이디 값을 추출
 
                 const temp_html = `
                     <li>
-                    <div class="column">
+                    <div class="column" data-ColumnId="${ColumnId}" >
                     <div class="column-header">
-                      <input type="text" class="column-name-input" id="Columns" value="${columnName}"/>
+                      <div class="column-name-input" id="Columns">${columnName}</div>
                       <button class="edit-column-btn" id="updateColumnName" value= "updateColumnName">수정</button>
                     </div>
                     <button class="add-card-btn">Add Card</button>
@@ -30,55 +31,50 @@ window.addEventListener("DOMContentLoaded", async function () {
     } catch (error) {
         console.error("Error fetching data:", error);
         console.log(error);
-    }
+    };
 
-    document
-    .getElementById("updateColumnName")
-    .addEventListener("click", async function () {
-      // 컬럼 아이디를 생성하는 로직 추가
-      const columnNameInput = document.getElementById("Columns");
-      const ColumnId = generateColumnId(columnNameInput.value);
-  
-      const ColumnNames = columnNameInput.value;
-      const newdata = JSON.stringify({
-        ColumnNames,
-      });
-  
-      console.log(newdata);
-  
-      fetch(`/api/columns/${ColumnId}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: newdata,
-      })
-        .then(response => {
-          if (response.ok) {
-            // 칼럼 이름 수정 성공
-            // 팝업 창으로 칼럼 수정 완료 메시지 전송
-            window.opener.postMessage("칼럼 이름", "*");
-            window.close();
-          } else {
-            // 칼럼 이름 수정 실패
-            alert("칼럼 이름 수정에 실패했습니다.");
-          }
-        })
-        .catch(error => {
-          console.error("칼럼 이름 수정 중 오류가 발생했습니다.", error);
-          alert("칼럼 이름 수정 중 오류가 발생했습니다.");
+    const columnsList = document.getElementById("Columns");
+    if (columnsList) {
+        columnsList.addEventListener('click', async (e) => {
+            if (e.target.classList.contains('edit-column-btn')) {
+                const columnElement = e.target.closest('.column');
+                const columnId = columnElement.getAttribute('data-ColumnId');
+
+                console.log(columnId);
+
+                const newColumnName = prompt('새로운 컬럼 이름을 입력하세요:');
+                if (!newColumnName) return;
+
+                const data = {
+                    columnName: newColumnName
+                };
+                try {
+                    const response = await updateColumn(`/api/columns/${columnId}`, data);
+                    const result = await response.json();
+                    if (result.message === '컬럼 이름이 업데이트되었습니다.') {
+                        // 업데이트 성공 시 추가 로직을 작성하세요.
+                    } else {
+                        alert(result.message);
+                    }
+                } catch (error) {
+                    console.error('컬럼 이름 업데이트 중 오류 발생:', error);
+                }
+            }
         });
-  });
-  
-  function generateColumnId(columnName) {
-    // 실제로는 더 복잡한 로직으로 컬럼 아이디를 생성해야 할 수도 있습니다.
-    // 이 예시에서는 간단하게 columnName을 이용하여 해시값을 생성합니다.
-    let hash = 0;
-    for (let i = 0; i < columnName.length; i++) {
-      hash = (hash << 5) - hash + columnName.charCodeAt(i);
-      hash |= 0; // Convert to 32bit integer
     }
-    return hash;
-  }
 
+    async function updateColumn(url, data) {
+        try {
+            const response = await fetch(url, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+            return response;
+        } catch (error) {
+            throw error;
+        }
+    }
 });
